f2c0d1413d222ab36d2fb070971cb2c972839100
Muhammed EMEN <emiremen001@gmail.com>
2022-07-03 00:54:55 +0300

    first commit

--- Assets/Scripts/PlanetScript.cs
+++ Assets/Scripts/PlanetScript.cs
@@ -4,26 +4,23 @@ using UnityEngine;
 
 public class PlanetScript : MonoBehaviour
 {
-    //Variables for the rotational speed of the planets and the rotational speed of the sun.
+    
     [SerializeField] float rotateSpeed, rotateAroundSpeed;
-    //Game manager script for get management.
-    [SerializeField] GameManager gameManager;
-    float travelledDistance, countTour;
+    float travelledDistance, tourCount;
+
+    [SerializeField] Transform rotatingTarget;
+
     void Update()
     {
-        //It makes the planet revolve around itself.
-        transform.Rotate(0, (rotateAroundSpeed / gameManager.simulationTime) * Time.deltaTime, 0);
-        //It makes the planet revolve around the sun.
-        transform.RotateAround(gameManager.sun.transform.position, Vector3.up, (rotateSpeed / gameManager.simulationTime) * Time.deltaTime);
+        transform.Rotate(0, rotateAroundSpeed * Time.deltaTime, 0);
+        transform.RotateAround(rotatingTarget.position, Vector3.up, rotateSpeed * Time.deltaTime);
 
-        //Calculates the travelled distance by planet with speed multiplier.
-        travelledDistance += (rotateSpeed / gameManager.simulationTime) * Time.deltaTime;
+        travelledDistance += rotateSpeed * Time.deltaTime;
         if (travelledDistance > 360)
         {
-            //If any planet completed its tour, resets the distance travelled.
             travelledDistance = 0f;
-            countTour++;
-            Debug.Log(gameObject.name + " has completed its tour. Tours count: "+countTour);
+         tourCount++;
+            Debug.Log(gameObject.name + "'s. Total tours: " + tourCount);
         }
     }
 }
